10c59e3659574505f4d971569ed6c3b0fb70975e
Stan <stan0men@yahoo.com>
2019-01-08 15:00:15 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp         = require('gulp');
+const browserSync  = require('browser-sync').create();
+const sass         = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const pug = require('gulp-pug');
+
+// Compile Sass & Inject Into Browser
+gulp.task('sass', function() {
+    return gulp.src(['src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest("dist/css"))
+        .pipe(browserSync.stream());
+});
+
+
+// Watch Sass & Serve
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "./src"  
+    });
+
+    gulp.watch(['src/scss/*.scss'], ['sass']);
+    gulp.watch("index.pug").on('change', browserSync.reload);
+});
+
+// Default Task
+gulp.task('default', ['serve']);
